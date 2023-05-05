92f054673618776026f2ca4ed978659605b79fbd
Vasiuk Kateryna <catherine.vasyuk11@gmail.com>
2023-05-06 01:41:47 +0300

    style edits

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -1,12 +1,5 @@
 import styled from 'styled-components';
 
-const FormContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
 const Form = styled.form`
   display: flex;
   flex-direction: column;
